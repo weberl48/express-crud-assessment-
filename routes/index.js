47ddabda1eed91a667c5b17a66c7705b5344e924
Lucas Weber <weberl48@outlook.com>
2015-07-23 11:08:12 -0600

    basic routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var db =require('monk')(process.env.MONGOLAG_URI || "localhost/articles");
+var articleCollection = db.get('articles');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//CREATE
+router.post('/articles', function(req,res,next){
+  var b = req.body;
+  articleCollection.insert({title:b.title, background:b.background, dark:b.dark, body:b.body,excerpt:b.excerpt});
+  res.redirect('/view');
 });
+//READ
+router.get('/', function(req,res,next) {
+  articleCollection.find({}, function(err,records){
+    res.render('index', {allArticles:records});
+  });
+});
+
+//READ
+router.get('/article/:id/show', function(req,res,next){
+  articleCollection.findOne({_id:req.params.id}, function(err,record){
+    console.log(record);
+  res.render('show',{Article:record});
+  });
+});
+//READ
+router.get('article/:id/edit', function(req,res,next) {
+  articleCollection.findOne({_id:req.params.id}, function(err,record){
+    console.log(record);
+  res.render('update',{Article:record});
+  });
+});
+
+router.get('/articles/new', function(req,res,next){
+  res.render('new');
+});
+
+
+
 
 module.exports = router;
